a1835adb2d5a804ae9e60e8a2f0292323920fbea
Ats <isfendiyar510@gmail.com>
2023-04-19 18:00:50 +0200

    Started new CRUD approach

---
+++ CSClub/Pages/Admin/CRUD/AddMember.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSClub.Pages.Admin;
+
+public class AddMemberModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ CSClub/Pages/Admin/CRUD/DeleteMember.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSClub.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSClub.Pages.Admin.CRUD;
+
+public class DeleteMemberModel : PageModel
+{
+    // Props
+
+    private readonly ApplicationDbContext _context;
+
+    public DeleteMemberModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult OnGet()
+    {
+
+        return RedirectToPage("/Admin/Dashboard");
+    }
+}
