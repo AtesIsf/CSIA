a9349f43ecdc22dbd36ad9cd96ba5e5f65ef23c3
Ats <isfendiyar510@gmail.com>
2023-07-26 23:27:43 +0300

    Finished Edit & Delete Logs

---
+++ CSClub/Pages/Admin/Logs/EditLog.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSClub.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSClub.Pages.Admin.Logs;
+
+public class EditLogModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    [BindProperty]
+    public LogModel? Log { get; set; }
+
+    // Ctors
+    public EditLogModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // Methods
+    public IActionResult OnGet(int? id)
+    {
+        if (id == null)
+            return RedirectToPage("/Teacher/Logs");
+
+        Log = _context.Logs.Find(id);
+
+        if (Log == null)
+            return RedirectToPage("/Teacher/Logs");
+
+        return Page();
+    }
+
+    public IActionResult OnPostSaveChanges()
+    {
+        var selectedLog = _context.Logs.Find(Log.Id);
+
+        if (selectedLog == null)
+            return RedirectToPage("/Error");
+
+        selectedLog.Date = Log.Date;
+        selectedLog.Entry = Log.Entry;
+        selectedLog.Attendance = Log.Attendance;
+
+        _context.SaveChanges();
+
+        return RedirectToPage("/Teacher/Logs");
+    }
+}
